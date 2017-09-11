6a6b0319aab19c2f4b0598531e911a4382f5f0ed
JOSHNAREDDYvignan <31817914+JOSHNAREDDYvignan@users.noreply.github.com>
2017-09-11 13:51:14 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+ public static void main(String[] args)
+  {
+	StudentGroup a=new StudentGroup[1];
+	Student s[]=new Student(id,fullName,birthDate,avgMark);
+	a s[].setStudents(101,sudheer,1997-19-28,66);
+   }
+
+}
